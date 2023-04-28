413958c88ca5750201ca460bdfea0d075145cd3e
Tony <admin@syntony666.com>
2023-04-29 02:31:48 +0800

    fix 404 page bug

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,16 +1,13 @@
 import Error404Component from "@/component/404";
 import ContainerComponent from "@/component/container";
-import Head from "next/head";
+import HeadComponent from "@/component/head";
 
 const title = "404";
 
 export default function Home404() {
   return (
     <>
-      <Head>
-        <title>{title}</title>
-        <meta property="og:title" content={title} key="title" />
-      </Head>
+      <HeadComponent title={title} />
       <ContainerComponent element={Error404Component()}></ContainerComponent>
     </>
   );
