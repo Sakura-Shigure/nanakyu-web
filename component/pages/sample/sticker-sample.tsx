52b8a42592f5e3da5989c0d6669b6d5ce114d59c
Tony <admin@syntony666.com>
2023-05-21 02:42:18 +0800

    add badge and add contact in notice

--- component/pages/sample/sticker-sample.tsx
+++ component/pages/sample/sticker-sample.tsx
@@ -10,8 +10,11 @@ export default function StickerSampleLayoutComponent({ data }: Prop) {
       <div className="ts-content ts-header is-large">貼圖模板</div>
       <div className="ts-grid is-compact tablet+:is-5-columns mobile:is-3-columns">
         {data.images.samples.template.map((img: string, i: number) => (
-          <div className="column ts-segment ts-image" key={i}>
+          <div className="column ts-segment is-dense ts-image" key={i}>
             <img src={img} alt="" />
+            <div className="ts-content is-center-aligned is-fitted">
+              <div className="ts-badge is-small is-outlined">{i + 1}</div>
+            </div>
           </div>
         ))}
       </div>
