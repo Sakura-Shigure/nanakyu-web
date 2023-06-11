3867f5f8ed2c26f02660fda3467b5c342fe0b0a1
Tony <admin@syntony666.com>
2023-06-11 22:53:57 +0800

    change og place

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,6 +12,13 @@ export default function App({ Component, pageProps }: AppProps) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
+        <meta property="og:title" content="奶加" />
+        <meta
+          property="og:description"
+          content="主接角色設計、L2D立繪、直播模板"
+        />
+        <meta name="description" content="主接角色設計、L2D立繪、直播模板" />
+        <meta property="og:image" content="/favicon.png" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/tocas/4.2.3/tocas.min.js" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
       </Head>
